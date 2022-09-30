24cca2942cfc8e998ec7b588eca88ea2d96a69d4
Marianadsm <mariana.dsm@hotmail.com>
2022-09-29 21:57:09 -0300

    Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+return(
+    <> 
+    <h1 className='titulo'>Home</h1>
+    <p><img className='img' src="https://cdn-icons-png.flaticon.com/512/4860/4860850.png" alt="Imagem tela inicial"/></p>
+    </>
+);
+
+}
+
+export default Home;
